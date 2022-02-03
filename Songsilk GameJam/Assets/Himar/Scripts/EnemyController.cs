6a5bab12b782fe2feeb3071c1eca4ae4f754f3b4
himar33 <himbrgo@iesvalldemossa.net>
2022-02-03 13:00:10 +0100

    Enemy raycast added

---
+++ Songsilk GameJam/Assets/Himar/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    Camera viewCamera;
+
+    void Start()
+    {
+        viewCamera = Camera.main;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
